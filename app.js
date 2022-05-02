c2cacb5de4a909f83f6127ea771f34ee65e16bc1
Pa Gutri <pagutri@gmail.com>
2022-05-02 11:57:59 -0500

    Add post method

--- app.js
+++ app.js
@@ -28,3 +28,9 @@ app.get('/v1/explorers/:id', (req, res) => {
     const explorer = {id: 1, name: "Carlo"}
     res.status(200).json(explorer)
 })
+
+app.post('/v1/explorers', (req, res) => {
+    console.log(`Api Explorers POST request ${new Date()}`)
+    const requestBody = req.body // Parámetros de un cliente
+    res.status(201).json({message: "Created"})
+})
