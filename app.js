91d9ec251980e31862e54134e51e07a9b6c350f9
Pa Gutri <pagutri@gmail.com>
2022-05-02 11:54:24 -0500

    Add new get method

--- app.js
+++ app.js
@@ -21,3 +21,10 @@ app.get('/v1/explorers', (req, res) => {
     const explorers = [explorer1, explorer2, explorer3, explorer4]
     res.status(200).json(explorers)
 })
+
+app.get('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers GET request ${new Date()}`)
+    console.log(`Getting explorer with id ${req.params.id}`)
+    const explorer = {id: 1, name: "Carlo"}
+    res.status(200).json(explorer)
+})
